87c6e66350b930627c8557e4bc381ae9af6ce1c4
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-04-23 15:05:37 +0100

    useState implementation

--- syne-app/src/App.jsx
+++ syne-app/src/App.jsx
@@ -1,21 +1,74 @@
-import React, {useEffect, useLayoutEffect} from 'react';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
 import "./App.css";
 import * as Tone from 'tone';
 
 function App() {
+  //UseStates for possible Synth options (envelope only atm)
+  const [attackState, setAttack] = useState(0.005);         //Default val: 0.005
+  const [decayState, setDecay] = useState(0.1);             //Default val: 0.1
+  const [sustainState, setSustain] = useState(0.3);         //Default val: 0.3
+  const [releaseState, setRelease] = useState(1);           //Default val: 1
+  const [oscillatorType, setOscType] = useState("triangle"); //Default val: triangle
+
+  //Synth Options (default)
+  const [options, setOptions] = useState({
+    oscillator: {
+        type: oscillatorType,
+        count: 3,
+        harmonicity: 1,
+        modulationFrequency: 0.4,
+        modulationIndex: 2,
+        modulationType: "square",
+        phase: 0,
+        spread: 20,
+        width: 0.2
+    },
+    envelope: {
+        attack: attackState,
+        decay: decayState,
+        sustain: sustainState,
+        release: releaseState
+    }
+  });
+
+  //Synth + Keys
+  const pianoKeys = document.querySelectorAll(".piano-keys .key");
+  const [synth, setSynth] = useState(new Tone.PolySynth(Tone.Synth).toDestination());
+
   //Character-to-number conversion
   const alphaVal = (s) => s.toLowerCase().charCodeAt(0) - 97
 
   const playKey = (note) => {
-    // const now = Tone.now();
-    // synth.triggerAttack(note, now);
-    // synth.triggerRelease(note, now + 1)
-    console.log(note);
+    const now = Tone.now();
+    synth.triggerAttack(note, now);
+    synth.triggerRelease(note, now + 0.25)
+    //console.log(note);
   }
 
-  //Synth + Keys
-  const synth = new Tone.PolySynth(Tone.Synth).toDestination();
-  const pianoKeys = document.querySelectorAll(".piano-keys .key");
+  const updateSynth = () => {
+
+    //Metallic ePiano
+    setSynth(new Tone.PolySynth(Tone.Synth, {
+      oscillator: {
+        type: "sawtooth6",
+        count: 3,
+        harmonicity: 1,
+        modulationFrequency: 0.01,
+        modulationIndex: 2,
+        phase: 0,
+        spread: 20,
+        width: 0.2
+      },
+      envelope: {
+        attack: 0.005,
+        decay: 0.1 ,
+        sustain: 0.3 ,
+        release: 1
+      }
+    }).toDestination());
+
+    console.log(synth);
+  }
 
   useLayoutEffect(() => {
     document.addEventListener("keydown", async () => {
@@ -34,6 +87,7 @@ function App() {
             <span>Show Keys</span><input type="checkbox" defaultChecked></input>
           </div>
       </header>
+      <button onClick={() => updateSynth()}>UPDATE SYNTH</button>
       <ul className="piano-keys">
           <li className="key white" key="C4" onClick={() => playKey("C4")}><span>C4</span></li>
           <li className="key black" key="C#4" onClick={() => playKey("C#4")}><span>C#4</span></li>

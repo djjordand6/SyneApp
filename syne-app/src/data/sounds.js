e12d8c483de6234d6204e2f2c4ed58238569ae17
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-04-25 16:47:12 +0100

    sounds.js Update

--- syne-app/src/data/sounds.js
+++ syne-app/src/data/sounds.js
@@ -1,5 +1,27 @@
 export default [
     {
+        keywords: "default",
+        oscillator: {
+            type: "triangle",
+            count: 3,
+            harmonicity: 1,
+            modulationFrequency: 0.4,
+            modulationIndex: 2,
+            modulationType: "square",
+            phase: 0,
+            spread: 20,
+            width: 0.2
+        },
+        envelope: {
+            attack: 0.005,
+            decay: 0.1 ,
+            sustain: 0.3 ,
+            release: 1
+        },
+        volume: -2,
+        id: 0
+    },
+    {
         keywords: "metallic epiano",
         oscillator: {
             type: "sawtooth6",
@@ -18,12 +40,12 @@ export default [
             release: 1
         },
         volume: -5,
-        id: 0
+        id: 1
     },
     {
-        keywords: "random",
+        keywords: "airy air soothe space",
         oscillator: {
-            type: "square",
+            type: "sawtooth",
             count: 3,
             harmonicity: 1,
             modulationFrequency: 0.01,
@@ -38,8 +60,13 @@ export default [
             sustain: 0.3 ,
             release: 1
         },
+        lfo: {
+            type: "sine",
+            frequency: 0.2,
+            amplitude: 0.02
+        },
         volume: -4,
-        id: 1
+        id: 2
     },
     {
         keywords: "metallic",
@@ -60,6 +87,6 @@ export default [
             release: 1
         },
         volume: -5,
-        id: 0
+        id: 3
     },
 ];

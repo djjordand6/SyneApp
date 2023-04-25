09ddabeba8ee8c21ddaa642aad1eecd6c943d99a
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-04-25 14:39:51 +0100

    name field changed to "keywords"

--- syne-app/src/data/sounds.js
+++ syne-app/src/data/sounds.js
@@ -1,6 +1,6 @@
 export default [
     {
-        name: "metallic epiano",
+        keywords: "metallic epiano",
         oscillator: {
             type: "sawtooth6",
             count: 3,
@@ -21,7 +21,7 @@ export default [
         id: 0
     },
     {
-        name: "random",
+        keywords: "random",
         oscillator: {
             type: "square",
             count: 3,
@@ -40,5 +40,26 @@ export default [
         },
         volume: -4,
         id: 1
-    }
+    },
+    {
+        keywords: "metallic",
+        oscillator: {
+            type: "sawtooth6",
+            count: 3,
+            harmonicity: 1,
+            modulationFrequency: 0.01,
+            modulationIndex: 2,
+            phase: 0,
+            spread: 20,
+            width: 0.2
+        },
+        envelope: {
+            attack: 0.005,
+            decay: 0.1 ,
+            sustain: 0.3 ,
+            release: 1
+        },
+        volume: -5,
+        id: 0
+    },
 ];
